30a77a185a23a7edeb9c7c936b48aa956799be9d
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-10 21:40:38 +0700

    Update button.py

--- assets/button.py
+++ assets/button.py
@@ -1,1 +1,22 @@
+from pyrogram.types import InlineKeyboardButton
 
+
+class BUTTON:
+    BBUAT = [InlineKeyboardButton("🔥Start Generating Session🔥", callback_data="generate")]
+
+    BACK = [
+        generate_single_button,
+        [InlineKeyboardButton(text="Kembali", callback_data="home")]
+    ]
+
+    generate_button = [BBUAT]
+
+    BHOME = [
+        BBUAT,
+        [InlineKeyboardButton("Support", url="https://t.me/DezetSupport")],
+        [
+            InlineKeyboardButton("Help", callback_data="help"),
+            InlineKeyboardButton("About", callback_data="about")
+        ],
+        [InlineKeyboardButton("OWNER", url="https://t.me/msdqqq")],
+    ]
