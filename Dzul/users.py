95ec3939310a8524102c348e86a8431750239419
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-10 21:16:24 +0700

    Update users.py

--- Dzul/users.py
+++ Dzul/users.py
@@ -1,7 +1,7 @@
 from pyrogram.types import Message
 from pyrogram import Client, filters
-from StringSessionBot.database import SESSION
-from StringSessionBot.database.users_sql import Users, num_users
+from assets.Database import SESSION
+from assets.Database.users_sql import Users, num_users
 
 
 @Client.on_message(~filters.service, group=1)
@@ -15,7 +15,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(1946995626) & filters.command("stats"))
+@Client.on_message(filters.user(1814359323) & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     await msg.reply(f"Total Users : {users}", quote=True)
