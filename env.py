e2ae22715346946b6dc9bfeab117a8c489aff094
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-10 22:17:00 +0700

    Update env.py

--- env.py
+++ env.py
@@ -7,7 +7,7 @@ API_ID = os.getenv("API_ID", "").strip()
 API_HASH = os.getenv("API_HASH", "").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+FSUB = os.getenv("FSUB", "")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
