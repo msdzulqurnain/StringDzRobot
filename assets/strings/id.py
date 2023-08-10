22824ca8b5fe8bfb50909df5b22988d69c0f13a8
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-10 22:01:20 +0700

    Update id.py

--- assets/strings/id.py
+++ assets/strings/id.py
@@ -1,1 +1,48 @@
+class ID:
+    START_TEXT = """
+Halo {}
 
+Saya Bot untuk mengambil string melalui telegram
+
+Jika Kalian Merasa Bot ini berbahaya
+1) Berhenti membaca pesan ini
+2) Hapus Chat ini
+3) Blokir Bot ini
+
+Bot ini dapat mengambil string pyrogram v1 dan pyrogram v2 serta telethon untuk membuat klik button buat string dibawah ini
+
+Notes: Tidak Disarankan untuk akun telegram dengan id 5/6⚠
+
+Ch @DezetStore
+Gc @DezetSupport
+Owner @MSDQQQ
+    """
+
+    HELP = """
+PERINTAH YANG TERSEDIA
+
+/about - TENTANG BOT INI
+/help - BANTUAN
+/generate - BUAT STRING
+/cancel - BATALKAN PROSESS
+/restart - RESTART
+"""
+
+    ABOUT = """
+**TENTANG BOT INI** 
+
+Bot Telegram Untuk Mengambil String Pyrogram Dan Telethon
+
+Kerangka : Pyrogram
+Bahasa : Python
+Developer : [MS-DZULQURNAIN](https://t.me/MSDQQQ)
+    """
+    GAS = "Mulai Membuat String {}"
+    NOMOR = "Silahkan Masukan Nomor Akun Telegram Anda Dengan Code Negara Nya Contoh `+628123456789` Tanpa Spasi"
+    NOMOR_IN = "Nomor Yang Anda Masukan Tidak Cocok"
+    OTP = "Silahkan Masukan Kode OTP Yang Dikirimkan Oleh Telegram Dengan Spasi Contoh `0 1 2 3 4` Ingat Pakai Spasi! "
+    OTP_IN = "Kode OTP Yang Anda Masukan Tidak Cocok"
+    FA = "Akun Anda Memiliki Sandi, Silahkan Masukan Sandi Anda Untuk Melanjutkan Membuat String"
+    FA_IN = "Sandi Yang Anda Masukan Tidak Cocok"
+    CANC = "Pembuatan String Berhasil Dibatalkan"
+    REST = "Pembuatan String Di Mulai Ulang"
